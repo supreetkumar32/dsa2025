90e1cf48d50a218e7c6eb5ce5e5b930c90b8fcde
Kumar Supreet (MS/EDX1-XC) <uuc3kor@bosch.com>
2025-01-07 21:15:54 +0530

    thik kiye

--- array/findallduplicates.java
+++ array/findallduplicates.java
@@ -6,14 +6,19 @@ public class findallduplicates {
 	//so 1 duplicate element is there 
 	//we have to find that duplicate element
 	public static void main(String[] args) {
-		int a[]= {1,2,3,4,4,5};
-        int sum=0;
-        int n=a.length;
-        for(int i=0;i<a.length;i++) {
-        	sum=sum+a[i];
-        }
-        int result=sum-(n*(n-1))/2;
-		System.out.println(result);
+		int a[]= {4,3,2,7,8,2,3,1};
+		List<Integer> l=new ArrayList<>();
+		HashSet<Integer> hs=new HashSet<>();
+		for(int i=0;i<a.length;i++) {
+			if(hs.contains(a[i])) {
+				l.add(a[i]);
+			}else {
+				hs.add(a[i]);
+			}
+		}
+		for(int x:l) {
+			System.out.println(x);
+		}
 	}
 	
 
