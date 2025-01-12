3fc41dd945890940ac0f548c49060ff50a9d7cb1
supreetkumar32 <77229569+supreetkumar32@users.noreply.github.com>
2025-01-12 16:38:39 +0530

    Create reverse.java

---
+++ recursion/reverse.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class reverseArray {
+	public static void reverse(int a[],int i,int j) {
+		
+		if(i>=j)
+			return;
+		int temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+		reverse(a,i+1,j-1);
+	}
+
+	public static void main(String[] args) {
+		int arr[]= {1,2,3,4,5};
+		int i=0,j=arr.length-1;
+		reverse(arr,i,j);
+        for(int z=0;z<arr.length;z++) {
+        	System.out.print(arr[z]+ " ");
+        }
+
+	}
+
+}
