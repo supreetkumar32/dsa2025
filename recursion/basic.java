7979ef8b06388785130ac1d46c1f649bb1cd3efd
supreetkumar32 <77229569+supreetkumar32@users.noreply.github.com>
2025-01-12 10:56:40 +0530

    Update basic.java

--- recursion/basic.java
+++ recursion/basic.java
@@ -2,7 +2,7 @@ package Recursion;
 
 public class basic {
 
-	public static void 	printTill1toN(int n) {
+	public static void printTill1toN(int n) {
 		if(n>5) return;
 		System.out.println(n);
 		printTill1toN(n+1);//it will print 1 to 4
@@ -11,9 +11,7 @@ public class basic {
 	public static void printFromNto1(int n) {
 		if(n<1) return;
 		System.out.println(n);
-		printFromNto1(n-1);//it will print from n to 1
-			
-		
+		printFromNto1(n-1);//it will print from n to 1		
 	}
 	public static void printFrom1ton(int n) {
 		if(n<1) return;		
@@ -21,7 +19,6 @@ public class basic {
 		System.out.println(n);	
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int n=5;
 		printTill1toN(n);
 		printFromNto1(n);
