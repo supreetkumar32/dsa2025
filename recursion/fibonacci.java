9f4493417ac696d748e45a8296019837d5683f27
supreetkumar32 <77229569+supreetkumar32@users.noreply.github.com>
2025-01-12 16:37:52 +0530

    Create fibonacci.java

---
+++ recursion/fibonacci.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class fibonacci {
+
+	//fibonacci 0  1 1 2 3 5
+	public static int fibo(int n) {
+		if(n<=1) return n;
+		return fibo(n-1)+fibo(n-2);
+	}
+	public static void main(String[] args) {
+		int n=5;
+		System.out.println(fibo(n));
+
+	}
+
+}
