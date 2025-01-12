61be496020f8d76026a991588528df5cc8fa0183
supreetkumar32 <77229569+supreetkumar32@users.noreply.github.com>
2025-01-12 16:38:16 +0530

    Create palindrome.java

---
+++ recursion/palindrome.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class palindrome {
+	public static boolean isPalindrome(String s,int i, int j) {
+		char[]c=s.toCharArray();
+		if(i>=j) return true;
+		if(c[i]!=c[j]) return false;
+		return isPalindrome(s,i+1, j-1);
+		
+	}
+
+	public static void main(String[] args) {
+		String s="momk";
+		int i=0,j=s.length()-1;;
+		System.out.println(isPalindrome(s,i,j));
+
+	}
+
+}
