1a0ddcb43e14976ac1920e566ab96362f475735a
Supreet <supreetkumar32@gmail.com>
2025-07-06 10:43:17 +0530

    basic q ended

--- patterns/Basic.java
+++ patterns/Basic.java
@@ -30,11 +30,63 @@ public class Basic {
 		}
 	}
 	
+	public static void pattern3(int n) {
+//		1 
+//		1 2 
+//		1 2 3 
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
 	
+	public static void pattern4(int n) {
+//		1 
+//		2 2 
+//		3 3 3 
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void pattern5(int n) {
+//		***
+//		**
+//		*
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n-i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void pattern6(int n) {
+//		1 2 3 
+//		1 2 
+//		1 
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n-i+1;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
 
 	public static void main(String[] args) {
 		//pattern1(3);
-		pattern2(3);
+		//pattern2(3);
+		//pattern3(3);
+		//pattern4(3);
+		//pattern5(3);
+		pattern6(3);
+//		pattern3(3);
+//		pattern3(3);
 
 	}
 
