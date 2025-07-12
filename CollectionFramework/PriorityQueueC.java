1eaea6018598145657d7b9e2b7122140577f65ef
Supreet <supreetkumar32@gmail.com>
2025-07-12 14:31:28 +0530

    priority queue

---
+++ CollectionFramework/PriorityQueueC.java
@@ -0,0 +1,27 @@
+package CollectionFramework;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueueC {
+
+	public static void main(String[] args) {
+	//default is the increasing order
+		 //Queue<Integer> pq = new PriorityQueue<>();
+        Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.offer(40);
+        pq.offer(12);
+        pq.offer(24);
+        pq.offer(36);
+
+        System.out.println(pq);
+        pq.poll();
+        System.out.println(pq);
+
+        System.out.println(pq.peek());
+
+	}
+
+}
